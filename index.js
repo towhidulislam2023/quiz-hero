a6bb8577c153c68116c237b81b8af27ae0905cef
towhidulislam2023 <towhidulislam2.bd@gmail.com>
2023-03-15 23:46:09 +0600

    All Done

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await(res.json()) ;
+  const data = await (res.json());
   quizData = data;
   displayQuiz(data);
 };
@@ -76,7 +76,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventListener("click", function() {
+document.querySelector("#submit").addEventListener("click", function () {
   if (answers.length < 6) {
     return;
   }
@@ -148,16 +148,15 @@ document.querySelector("#submit").addEventListener("click", function() {
     </h1>
     <p class="text-sm flex justify-center items-center gap-2">
       Total Time: <span class="text-xl text-orange-500">${timeTaken.innerText.replace(
-        "sec",
-        ""
-      )}<span class="text-xs">sec</span></span>
+      "sec",
+      ""
+    )}<span class="text-xs">sec</span></span>
     </p>
   </div>
   
   <button onclick="location.reload();" class="bg-green-600 text-white w-full py-2 rounded mt-16">Restart</button>
-  ${
-    storage
-      ? `<div class="mt-5">
+  ${storage
+        ? `<div class="mt-5">
       <h1 class="text-center">Previous Submissions <button class="text-blue-800 text-xs" onclick={localStorage.clear();location.reload()}>Clear History</button></h1>
     <div
     class="flex justify-between items-center border rounded p-2 my-2 shadow-sm font-medium">
@@ -166,18 +165,18 @@ document.querySelector("#submit").addEventListener("click", function() {
     <div>Time</div>
     </div>
     ${storage
-      ?.reverse()
-      ?.map(
-        (item) => `<div
+          ?.reverse()
+          ?.map(
+            (item) => `<div
       class="flex justify-between items-center border rounded p-2 my-2 shadow-sm">
       <div>${item.marks}/60</div>
       <div>${item.status}</div>
       <div>${item.examTime}</div>
       </div>`
-      )
-      ?.join("")}`
-      : ""
-  }
+          )
+          ?.join("")}`
+        : ""
+      }
   </div>
   `;
 
@@ -186,6 +185,6 @@ document.querySelector("#submit").addEventListener("click", function() {
   window.scrollTo(0, 0);
 });
 document.getElementById('blogBtn').addEventListener("click", function () {
-   location.href="./blog.html"
-  
+  location.href = "./blog.html"
+
 })
