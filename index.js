307573fda535414c3ae5073ef3486914f13e4a28
towhidulislam2023 <towhidulislam2.bd@gmail.com>
2023-03-15 20:51:11 +0600

    display-que and fixed undefined que

--- index.js
+++ index.js
@@ -60,13 +60,13 @@ const displayQuiz = (data) => {
   }
 
   data.forEach((quiz, i) => {
-    console.log(quiz);
+    console.log(quiz.question);
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
@@ -76,7 +76,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", function() {
+document.querySelector("#submit").addEventListener("click", function() {
   if (answers.length < 6) {
     return;
   }
@@ -110,7 +110,7 @@ document.querySelector("#submit").addEventlistener("click", function() {
   }
 
   // data setting on local storage and getting data from local storage
-  let storage = JSON.parse(localStorage.getItem("result"));
+  let storage = JSON.parse(localStorage.getItem("results"));
   if (storage) {
     localStorage.setItem(
       "results",
